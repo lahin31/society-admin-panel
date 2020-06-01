33c9e8cd3c3222c474c3a05f601e6985efa58ef5
MuhammadLahin <muhammad.lahin@gmail.com>
2020-06-01 22:53:55 +0600

    client side login page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    
+    <Router>
+      <main className="main-content">
+        <Switch>
+          <Route path="/login" component={Login} />
+        </Switch>
+      </main>
+    </Router>
   )
 }
 
